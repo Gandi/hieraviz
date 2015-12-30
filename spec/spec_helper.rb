05adf1dd9293da3127eb31a29ec2268ecf871c25
mose <mose@gandi.net>
2015-12-30 12:11:16 +0800

    fix link to coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,28 @@ require 'rspec'
 ENV['RACK_ENV'] = 'test'
 ENV['HIERAVIZ_CONFIG_FILE'] = File.expand_path '../files/config.yml', __FILE__
 
+if !ENV['BUILD']
+  require 'rubygems'
+  require 'bundler'
+
+  if ENV['COV']
+    require 'simplecov'
+    SimpleCov.profiles.define :app do
+      add_group 'bin', '/bin'
+      add_group 'app', '/app'
+      add_group 'lib', '/lib'
+      add_filter '/vendor/'
+      add_filter '/spec/'
+    end
+    SimpleCov.start :app
+  else
+    require 'coveralls'
+    Coveralls.wear!
+    # require "codeclimate-test-reporter"
+    # CodeClimate::TestReporter.start
+  end
+end
+
 require File.expand_path '../../app/main.rb', __FILE__
 
 module RSpecMixin
