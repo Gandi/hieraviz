0a42a02010e0ef72c663d33dcf50436a3add6db7
mose <mose@mose.com>
2016-01-07 16:52:50 +0800

    prepare auth_gitlab tests

---
+++ spec/lib/auth_gitlab_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe Hieraviz::AuthGitlab do
+
+  describe '.new' do
+  end
+
+  describe '.access_token' do
+  end
+
+  describe '.get_response' do
+  end
+
+  describe '.redirect_uri' do
+  end
+
+  describe '.login_url' do
+  end
+
+  describe '.authorized?' do
+  end
+
+  describe '.user_info' do
+  end
+
+end
