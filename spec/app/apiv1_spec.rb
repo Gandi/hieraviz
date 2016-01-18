5491fd3eeefdfc3688c7abb45e977c0cf33a45b1
mose <mose@gandi.net>
2016-01-18 14:26:30 +0800

    fixing test

--- spec/app/apiv1_spec.rb
+++ spec/app/apiv1_spec.rb
@@ -3,10 +3,12 @@ require 'sinatra_helper'
 
 describe HieravizApp::ApiV1 do
 
-  context "when GET /v1/nodes" do
-    it "replies 200" do
-      get '/nodes'
-      expect(last_response).to be_ok
+  context "without any auth" do
+    context "when GET /v1/nodes" do
+      it "replies error" do
+        get '/nodes'
+        expect(last_response).not_to be_ok
+      end
     end
   end
 
