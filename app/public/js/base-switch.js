09b36bdaed906bba4e6737b64143750054d512a1
mose <mose@mose.com>
2016-01-28 11:49:06 +0800

    manage js for base switching

--- app/public/js/base-switch.js
+++ app/public/js/base-switch.js
@@ -1,12 +1,25 @@
 
 ready( () => {
   var bases = document.querySelectorAll('.base .all .select');
+  var button = document.querySelector(".base .current");
+  var menu = document.querySelector(".base .all");
+  var menuvisible = false;
   filterBox(".base .all .filter input", bases);
+
+  button.addEventListener('click', (ev) => {
+    if (menuvisible) {
+      removeClass(menu, "focus");
+    } else {
+      addClass(menu, "focus");
+    }
+    menuvisible = !menuvisible;
+  });
 
   /* declaration of events for the bases menu */
   Array.prototype.forEach.call(bases, (item, i) => {
     item.addEventListener('click', (ev) => {
-      
+      var url = window.location;
+      url.pathname = url.pathname.replace(/^\/[^\/]+/,"/"+ev.target.textContent);
     });
   });
 
