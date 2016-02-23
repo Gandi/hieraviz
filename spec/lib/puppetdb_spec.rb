3345f810d32c5663aa66c5a2c07a89c0e249b872
mose <mose@mose.com>
2016-02-23 10:36:14 +0800

    some more tests

---
+++ spec/lib/puppetdb_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe Hieraviz::Puppetdb do
+
+  let(:ppdb) { Hieraviz::Puppetdb.new Hash.new }
+
+  describe '.new' do
+    it { expect(ppdb.instance_variable_get(:@request)).to be_a Hieracles::Puppetdb::Request }
+  end
+
+  describe '.events' do
+    before do
+      allow_any_instance_of(Hieracles::Puppetdb::Request).
+        to receive(:events).
+        and_return('something')
+    end
+    it { expect(ppdb.events).to eq 'something' }
+  end
+
+end
